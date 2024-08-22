6ad0f80ae1124d00b7cb065023dcd6e1b5cdba1c
Júlia Corrêa <juliacorrea@MacBook-Air-de-Julia.local>
2024-08-22 16:39:02 -0300

    variaveis operadores

---
+++ atividade1 /variaveisOperadores.js
@@ -0,0 +1,12 @@
+let n1 = 2;
+let n2 = 3;
+
+let soma = n1 + n2;
+let subtração = n1 - n2;
+let multiplicação = n1 * n2;
+let divisão = n1 / n2;
+
+console.log(`o resultado da soma entre ${n1} e ${n2} é ${soma}`);
+console.log(`o resultado da subtração entre ${n1} e ${n2} é ${subtração}`);
+console.log(`o resultado da multiplicação entre ${n1} e ${n2} é ${multiplicação}`);
+console.log(`o resultado da divisão entre ${n1} e ${n2} é ${divisão}`);
